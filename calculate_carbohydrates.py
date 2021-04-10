68f8086c3b1fe3407dedd39a10f74bd4d3a31103
Grigory <Rygor83@users.noreply.github.com>
2021-04-10 15:57:22 +0300

    0.2: корректировка чисел
    Корректировка числа при вводе. Если пользователь ввел число с запятой,
    то запятую меняем еа точку.

--- calculate_carbohydrates.py
+++ calculate_carbohydrates.py
@@ -1,6 +1,4 @@
 import ui
-from datetime import datetime
-import requests, json
 
 #################################
 # Views
@@ -27,7 +25,6 @@ tf_carb_per_100.height = 35
 tf_carb_per_100.keyboard_type = ui.KEYBOARD_NUMBERS
 tf_carb_per_100.border_width = 1
 
-
 # Label: выбор расчета
 lb_option = ui.Label()
 lb_option.text = ' 2. Выберите расчет:'
@@ -45,9 +42,8 @@ sc_option.x = 0
 sc_option.y = 110
 sc_option.width = screen_width
 sc_option.height = 50
-sc_option.segments = 'Сколько ХЕ в Х грамм?','Сколько гр.прод. на Х ХЕ?'
+sc_option.segments = 'Сколько ХЕ в Х грамм?', 'Сколько гр.прод. на Х ХЕ?'
 sc_option.border_width = 1
-
 
 # Label: во что пересчитываем
 lb_param_to = ui.Label()
@@ -68,7 +64,6 @@ tf_param_to.height = 35
 tf_param_to.keyboard_type = ui.KEYBOARD_NUMBERS
 tf_param_to.border_width = 1
 
-
 # Button: convert
 bt_convert = ui.Button()
 bt_convert.border_width = 4
@@ -77,7 +72,7 @@ bt_convert.x = 0
 bt_convert.y = 250
 bt_convert.width = screen_width
 bt_convert.height = 50
-bt_convert.font = ('verdana',25)
+bt_convert.font = ('verdana', 25)
 bt_convert.corner_radius = 20
 
 # TextView: результат перерасчета
@@ -94,68 +89,69 @@ txtv_info.border_width = 1
 # Global class
 #################################
 
+
 class MyClass(ui.View):
-	
 	def __init__(self, *args, **kwargs):
 		super().__init__(*args, **kwargs)
 		self.table1_selection = None
 		self.bg_color = 'lightyellow'
 		self.make_view()
-		
+
 	def make_view(self):
-		# self.add_subview(dp_rep_date)
+		# добавление элементов на экран
 		self.add_subview(sc_option)
 		self.add_subview(lb_option)
 		self.add_subview(lb_carb_per_100)
 		self.add_subview(tf_carb_per_100)
 		self.add_subview(lb_param_to)
 		self.add_subview(tf_param_to)
-		
+
 		self.add_subview(bt_convert)
 		self.add_subview(txtv_info)
-		
+
 		# Actions
-		
-		tf_carb_per_100.action = self.fn_carb_per_100_entered
-		
 		sc_option.action = self.fn_segment_select
-		
 		bt_convert.action = self.fn_convert
-		
-	def fn_carb_per_100_entered(self, sender):
-		p = sender.text
-		
+
 	def fn_segment_select(self, sender):
 		if sc_option.selected_index == 0:
 			lb_param_to.text = " 3. Сколько грамм продукта даем:"
 		elif sc_option.selected_index == 1:
 			lb_param_to.text = " 3. Сколько ХЕ нужно дать:"
-		
+
 	def fn_convert(self, sender):
-		
+
+		# Проверка на заполнение полей
 		if tf_carb_per_100.text == '':
 			txtv_info.text = '1. Заполните сколько гр. углеводов в 100 г продукта'
-		elif sc_option.selected_index ==  -1:
+		elif sc_option.selected_index == -1:
 			txtv_info.text = '2. Выберите расчет'
-		elif  tf_param_to.text == '':
+		elif tf_param_to.text == '':
 			txtv_info.text = '3. Заполните сколько даем грамм / ХЕ продукта'
 		else:
+			# Меняем запятую в числе на точку
+			tf_param_to.text = tf_param_to.text.replace(',','.')
+			tf_carb_per_100.text = tf_carb_per_100.text.replace(',','.')
+			
+			# Расчет
 			# Сколько грамм продукта даем
 			if sc_option.selected_index == 0:
-				
-				amount = int(tf_param_to.text) * int(tf_carb_per_100.text) / 100 / 12
-				
-				txtv_info.text = f'{round(amount,2)} ХЕ в {tf_param_to.text} грамм(а) продукта'
-				
+
+				amount = float(tf_param_to.text) * float(tf_carb_per_100.text) / 100 / 12
+
+				ans = f'{round(amount,2)} ХЕ в {tf_param_to.text} грамм(а) продукта'
+
 			# Сколько ХЕ нужно дать
 			elif sc_option.selected_index == 1:
-				
-				amount = int(tf_param_to.text) * 12 * 100 / int(tf_carb_per_100.text)
-				
-				txtv_info.text = f'{round(amount,2)} грамм(а) продукта даём на {tf_param_to.text} ХЕ'
-		
+
+				amount = float(tf_param_to.text) * 12 * 100 / float(tf_carb_per_100.text)
+
+				ans = f'{round(amount,2)} грамм(а) продукта даём на {tf_param_to.text} ХЕ'
+
+			txtv_info.text = ans
+
 ##################
 # Зауск
 ##################
-v = MyClass(name='Carbohydrate converter')
+v = MyClass(name='Калькулятор углеводов')
 v.present('full_screen')
